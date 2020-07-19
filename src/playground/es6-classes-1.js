61d5d27be8398c01d8cdc1b687de29ead7a9a288
Meng Zhang <547239782@qq.com>
2020-07-19 21:44:42 +1000

    Finished P25

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -11,8 +11,45 @@ class Person {
   }
 }
 
-const me = new Person('Andrew Mead', 26);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += `Their major is ${this.major}`
+    }
+    return description;
+  }
+}
+
+class Traveller extends Person {
+  constructor(name, age, homeLocation) {
+    super (name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  hasHomelocation () {
+    return !!this.homeLocation
+  }
+
+  getGretting () {
+    let gretting = super.gretting;
+    if (this.hasHomelocation) {
+      gretting += `Hi. I am Meng Zhang. I'm visiting from ${this.homeLocation}`
+    }
+    return gretting;
+  }
+}
+
+const me = new Student('Andrew Mead', 26, 'Computer Science');
 console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getDescription());
+const other = new Student();
+console.log(other.hasMajor);
