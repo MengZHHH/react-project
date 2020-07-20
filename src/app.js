6411f2c577f2c1cdab54208721ad1477e9dedc82
Meng Zhang <547239782@qq.com>
2020-07-20 15:08:43 +1000

    Commits after finishing components

---
+++ src/app.js
@@ -0,0 +1,69 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return(
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOptions />
+      </div>
+    )
+  }
+}
+class Header extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Indecistion</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    )
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>
+          What should I do?
+        </button>
+      </div>
+    )
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return(
+      <div>
+        Options component here
+        <Option />
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return(
+      <div>
+        Option
+      </div>
+    )
+  }
+}
+
+class AddOptions extends React.Component {
+  render() {
+    return(
+      <div>
+        <form>
+          <input type="text" id="firstname" name="firstname"></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
