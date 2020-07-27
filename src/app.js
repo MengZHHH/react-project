1b7250ae85ec7f61eaad65d6637cb57c488528d4
Meng Zhang <547239782@qq.com>
2020-07-27 20:49:45 +1000

    default export

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
-import { square, add } from './utils.js'
-import { isAdult, canDrink } from './person.js'
+import subtract, { square, add } from './utils.js'
 console.log('APpjs is running!11')
 
 console.log(square(4));
 console.log(add(4, 4));
+console.log(subtract(4, 4));
+
+
+import aaa, { isAdult, canDrink } from './person.js'
 
 console.log(isAdult(18));
 console.log(canDrink(18));
+console.log(aaa(18));
--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,7 @@ export const square = (x) => x * x;
 
 export const add = (a, b) => a + b;
 
-// export { square, add }; 
+export default (a,b) => a - b;
+// export { square, add, subtract as default }; 
 
 //exports - default export - named export
