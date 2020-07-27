e1988592434cebfbbaa0cee291771e16fd9285e7
Meng Zhang <547239782@qq.com>
2020-07-27 20:34:41 +1000

    first import

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+import { square, add } from './utils.js'
+import { isAdult, canDrink } from './person.js'
 console.log('APpjs is running!11')
+
+console.log(square(4));
+console.log(add(4, 4));
+
+console.log(isAdult(18));
+console.log(canDrink(18));
